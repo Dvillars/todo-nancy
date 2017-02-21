3fdd36c1ae10269c989c39467912ec514997d1b7
sandro mijatovic <mija.sand@gmail.com>
2017-02-20 16:35:21 -0800

    final

---
+++ Startup.cs
@@ -0,0 +1,7 @@
+namespace ToDoList
+{
+    public static class DBConfiguration
+    {
+        public static string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo;Integrated Security=SSPI;";
+    }
+}
---
+++ Tests/TaskTest.cs
@@ -0,0 +1,61 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ToDoList
+{
+  public class ToDoTest : IDisposable
+  {
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      int result = Task.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_Equal_ReturnsTrueIfDescriptionsAreTheSame()
+    {
+      Task firstTask = new Task("Mow the lawn");
+      Task secondTask = new Task("Mow the lawn");
+
+      Assert.Equal(firstTask, secondTask);
+    }
+
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      Task testTask = new Task("Mow the lawn");
+      testTask.Save();
+      List<Task> result = Task.GetAll();
+      List<Task> testList = new List<Task>{testTask};
+
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Find_FindsTaskInDatabase()
+    {
+      Task testTask = new Task("Mow the lawn");
+      testTask.Save();
+
+      Task foundTask = Task.Find(testTask.GetId());
+
+      Assert.Equal(testTask, foundTask);
+    }
+
+    public ToDoTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Task.DeleteAll();
+    }
+  }
+}
